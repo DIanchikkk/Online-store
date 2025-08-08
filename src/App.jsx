5a891124540edb96b698fc9dc51f9fb306ad4cd6
Diana <diana_yusupovaa@mail.ru>
2025-08-08 23:17:52 +0300

    added Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,30 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import './styles/App.css';
 
-import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
-import { DiscountNotice } from './components/DiscountNotice/DiscountNotice';
+import { Layout } from './components/Layout/Layout';
 
 import { Home } from './pages/Home/Home';
 import { Discounts } from './pages/Discounts/Discounts';
 import { Catalog } from './pages/Catalog/Catalog';
 import { Cart } from './pages/Cart/Cart';
 
-
-
 export const App = () => {
   return (
     <Provider store={store}>
       <div className="app">
         <Router>
-          <Header />
-          <DiscountNotice />
-
-          <main className="app__main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/discounts" element={<Discounts />} />
-              <Route path="/catalog" element={<Catalog />} />
-              <Route path="/cart" element={<Cart />} />
-            </Routes>
-          </main>
-
-          <Footer />
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="discounts" element={<Discounts />} />
+              <Route path="catalog" element={<Catalog />} />
+              <Route path="cart" element={<Cart />} />
+            </Route>
+          </Routes>
         </Router>
       </div>
     </Provider>
   );
 };
 
-export default App; 
+export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
+import { DiscountNotice } from "../DiscountNotice/DiscountNotice";
+
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <DiscountNotice />
+      <main className="app__main">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
